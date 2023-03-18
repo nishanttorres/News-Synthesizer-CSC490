b7c16c37154f1688168e7a64c8ab9349c38ee327
Juan Luna <juanluna24680@gmail.com>
2023-03-18 02:14:11 -0500

    Made changes to home.jsx and package.json

--- sample/news-synthesizer-jsx/src/home.jsx
+++ sample/news-synthesizer-jsx/src/home.jsx
@@ -2,27 +2,37 @@ import DatePicker, { DateObject } from "react-multi-date-picker"
 import DatePanel from "react-multi-date-picker/plugins/date_panel"
 import { useState, useMemo } from 'react';
 import Select from 'react-select'
-import countryList from 'react-select-country-list'
+//import countryList from 'react-select-country-list'
 import { createConfig } from "./config-creator";
+import APIService from "./component/APIService";
 import './css-styling/home.css'
 
 const Home = ({ changePage }) => {
-
+    
+    //sending dates, location, and category to flask
+    const insertFilters = () =>{
+        APIService.insertFilters(dateValue,locationValue,categoryValue)
+        .catch(error => console.log('error', error))
+    }
+    
     const today = new DateObject()
     const tmrw = new DateObject()
 
     tmrw.add(1, "days");
 
     const [dateValue, dateSetValue] = useState([today, tmrw]);
-    const [locationValue, locationSetValue] = useState('United States');
-    const [categoryValue, categorySetValue] = useState('POLITICS');
+    const [locationValue, locationSetValue] = useState('');
+    const [categoryValue, categorySetValue] = useState('');
     const userValue = { dateValue, locationValue, categoryValue };
 
-    const options = useMemo(() => countryList().getData(), [])
+    const options = require('./data-s/countries.json')
     const cat = require('./data-s/news_category.json')
+
     const config = () => {
         changePage();
         createConfig(userValue);
+        insertFilters();
+        
     }
     console.log(typeof dateValue)
     console.log(typeof locationValue)
