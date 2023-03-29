e6b703ede8b9eab92c771ed454a474b2ed0cf164
jluna327 <juanluna24680@gmail.com>
2023-03-29 14:55:53 -0500

    Changes to app.py
    app.py starts up the React app and the Flask server

--- sample/app.py
+++ sample/app.py
@@ -1,39 +1,49 @@
 import os
 import requests
+import subprocess
+import json
 import openai
-from text_processor import process_text
-from mimic3 import synthesize_speech
+from flask import request
+# from text_processor import process_text
+# from mimic3 import synthesize_speech
 from summarizer import summarize_article
-from perigon import fetch_articles
+# from perigon import fetch_articles
 
-# Set up your API keys
-PERIGON_API_KEY = os.environ["PERIGON_API_KEY"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+# # Set up your API keys
+# PERIGON_API_KEY = os.environ["PERIGON_API_KEY"]
+# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-# Configure OpenAI API
-openai.api_key = OPENAI_API_KEY
+# # Configure OpenAI API
+# openai.api_key = OPENAI_API_KEY
 
 
 def main():
-    # Get user input
-    date = input("Enter the date: ")
-    topic = input("Enter a topic: ")
-    duration = int(input("Enter duration (in minutes, multiples of 5): "))
+    react_dir = "./news-synthesizer-jsx"
+    subprocess.Popen(["npm", "start"], cwd=react_dir)
+    
+    with open("./flask/server.py") as f:
+        exec(f.read())
 
-    # Fetch articles
-    articles = fetch_articles(topic)
+    
+    # # Get user input
+    # date = input("Enter the date: ")
+    # topic = input("Enter a topic: ")
+    # duration = int(input("Enter duration (in minutes, multiples of 5): "))
 
-    # Summarize articles
-    summaries = [summarize_article(article) for article in articles]
+    # # Fetch articles
+    # articles = fetch_articles(topic)
 
-    # Process text and create SSML formatted text file
-    ssml_text = process_text(summaries)
-    with open("ssml_output.txt", "w") as f:
-        f.write(ssml_text)
+    # # Summarize articles
+    # summaries = [summarize_article(article) for article in articles]
 
-    # Synthesize speech using Mimic3
-    output_filename = f"output_{date}.wav"
-    synthesize_speech("ssml_output.txt", output_filename)
+    # # Process text and create SSML formatted text file
+    # ssml_text = process_text(summaries)
+    # with open("ssml_output.txt", "w") as f:
+    #     f.write(ssml_text)
+
+    # # Synthesize speech using Mimic3
+    # output_filename = f"output_{date}.wav"
+    # synthesize_speech("ssml_output.txt", output_filename)
 
 
 if __name__ == "__main__":
